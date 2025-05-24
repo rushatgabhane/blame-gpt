9047c19bdf5b52b8f90ea8964f60b9a82b6219e0
Rushat Gabhane <rushatgabhane@gmail.com>
2025-05-25 00:34:31 +0200

    Revert "add demo for issue"
    This reverts commit d059f1f438b113cf571f15cb8a7d1038ac668606.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,42 +16,6 @@ function App() {
         For a <span style={{ fontWeight: 'bold' }}>demo</span>, reach out on{' '}
         <a href="mailto:rushatgabhane@gmail.com">[rushatgabhane@gmail.com]</a>
       </p>
-      <h2>Demo</h2>
-      <p>
-        Possible culprit PRs for issue -{' '}
-        <a href="https://github.com/Expensify/App/issues/62685">
-          Expense - Next step action shows skeleton loading when workflows are
-          disabled
-        </a>
-      </p>
-      <ul>
-        <li>
-          <p>
-            <a href="https://github.com/Expensify/App/pull/62363">PR #62363</a>:
-            This PR adds loading skeletons for Next steps and Report Actions in
-            report screens, which could be related to the skeleton loading issue
-            when the workflows are disabled after submitting an expense.
-          </p>
-        </li>
-        <li>
-          <p>
-            <a href="https://github.com/Expensify/App/pull/58020">PR #58020</a>:
-            This PR changes components related to report actions and includes
-            the MoneyReportHeader component, which may impact the displaying of
-            next step actions after submitting an expense.
-          </p>
-        </li>
-        <li>
-          <p>
-            <a href="https://https://github.com/Expensify/App/pull/61170">
-              PR #61170
-            </a>
-            : This PR introduces changes to report action items and could
-            influence the visibility and interactions with the expense
-            submission process.
-          </p>
-        </li>
-      </ul>
     </div>
   );
 }
