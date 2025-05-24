a4bae72a6120180d63801d6302460e3065c394de
Rushat Gabhane <rushatgabhane@gmail.com>
2025-05-24 23:59:18 +0200

    add demo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,17 @@ import './App.css';
 function App() {
   return (
     <div id="main">
-      <h1 style={{ marginBottom: '20px', marginTop: '80px' }}>Blame GPT</h1>
-      <p>Finds the PRs begging to be reverted. Fast. </p>
+      <h1 style={{ marginBottom: '20px', marginTop: '80px' }}>BlameGPT</h1>
+      <p>Finds the pull request begging to be reverted. Fast. </p>
       <h2 id="software">Why is my shiny new feature not on production yet?</h2>
       <p>Oh... we have deploy blockers!!</p>
       <p>
         BlameGPT finds the PR causing a deploy blocker so you can go back to
         shipping. (Blame the PR, not your coworker. Probably.)
       </p>
+      <p>BlameGPT requires zero config.</p>
       <p>
-        BlameGPT requires zero config. Reach out on{' '}
+        For a <span style={{ fontWeight: 'bold' }}>demo</span>, reach out on{' '}
         <a href="mailto:rushatgabhane@gmail.com">[rushatgabhane@gmail.com]</a>
       </p>
     </div>
