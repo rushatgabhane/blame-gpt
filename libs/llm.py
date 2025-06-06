6c1c6d093953e713d3f05654ed4370aae8bd8be4
Rushat Gabhane <rushatgabhane@gmail.com>
2025-06-06 04:38:49 +0200

    get embeddings

--- libs/llm.py
+++ libs/llm.py
@@ -3,6 +3,4 @@ from langchain_openai import OpenAIEmbeddings
 import os
 
 llm = ChatOpenAI(model="gpt-4.1-mini", api_key=os.getenv("OPENAI_API_KEY"), temperature=0.2)
-embedding_model = OpenAIEmbeddings(
-    model="text-embedding-3-large", api_key=os.getenv("OPENAI_API_KEY")
-)
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-large", api_key=os.getenv("OPENAI_API_KEY"))
