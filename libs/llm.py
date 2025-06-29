15edc5daed7419fe3630df40e032edc685e387c0
Rushat Gabhane <rushatgabhane@gmail.com>
2025-06-29 03:09:40 +0200

    use o3-mini

--- libs/llm.py
+++ libs/llm.py
@@ -2,5 +2,5 @@ from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 import os
 
-llm = ChatOpenAI(model="gpt-4.1-mini", api_key=os.getenv("OPENAI_API_KEY"), temperature=0.2)
+llm = ChatOpenAI(model="o3-mini-2025-01-31", api_key=os.getenv("OPENAI_API_KEY"))
 embedding_model = OpenAIEmbeddings(model="text-embedding-3-large", api_key=os.getenv("OPENAI_API_KEY"))
