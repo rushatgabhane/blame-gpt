802cd206045c76e4b237e0b0cf2524c70135cc9f
Rushat Gabhane <rushatgabhane@gmail.com>
2025-07-01 21:34:46 +0200

    rm webhook

--- controllers/blame_controller.py
+++ controllers/blame_controller.py
@@ -21,37 +21,6 @@ class ManualBlameRequest(BaseModel):
     issue_id: int
 
 
-# This is a webhook endpoint that listens for GitHub events.
-@blame_router.post("/api/webhook")
-async def blame(request: Request, x_hub_signature_256: str = Header(None)):
-    body = await request.body()
-    if not helpers.is_valid_signature(x_hub_signature_256, os.getenv("GITHUB_WEBHOOK_SECRET") or "", body):
-        return Response(status_code=403, content="Invalid signature")
-
-    payload = json.loads(body)
-    if payload.get("action") != "labeled":
-        return Response(status_code=200, content="action is not 'labeled'")
-
-    if payload.get("label").get("name") != constants.LABELS["DeployBlockerCash"]:
-        return Response(status_code=200, content="label is not 'DeployBlockerCash'")
-
-    repository_name = payload.get("repository").get("name")
-    repository_owner = payload.get("repository").get("owner").get("login")
-
-    if repository_name != constants.REPO_NAME or repository_owner != constants.REPO_OWNER:
-        return Response(
-            status_code=200,
-            content=f"this repository is not supported",
-        )
-
-    issue = payload.get("issue")
-    issue_number = issue.get("number")
-    db = cast(Database, request.app.state.db)
-
-    asyncio.create_task(run_and_log_blame_pipeline(issue_number, db))
-    return Response(status_code=200, content=f"blame process started for #{issue_number}")
-
-
 @blame_router.post("/api/blame", dependencies=[Depends(auth_middleware.verify_user_auth_token)])
 async def blame_manual(request: Request, data: ManualBlameRequest):
     db = cast(Database, request.app.state.db)
