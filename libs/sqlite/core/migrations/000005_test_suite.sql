526e8d2780c56b1ef37f59823db6ac926b1699e8
Rushat Gabhane <rushatgabhane@gmail.com>
2025-07-14 07:58:44 +0300

    add test suite db

---
+++ libs/sqlite/core/migrations/000005_test_suite.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS test_suite (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    case_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    steps TEXT NOT NULL,
+    hash TEXT NOT NULL,
+    embedding TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
