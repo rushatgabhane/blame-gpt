70b838fdc07778136d53cc6d94186090f7425941
Rushat Gabhane <rushatgabhane@gmail.com>
2025-07-14 07:58:26 +0300

    add std llm

--- libs/llm.py
+++ libs/llm.py
@@ -10,10 +10,10 @@ if not api_key.get_secret_value():
 
 llmReasoningCheap = ChatOpenAI(model="o3-mini-2025-01-31", api_key=api_key)
 llmReasoning = ChatOpenAI(model="o3-2025-04-16", api_key=api_key)
+llm = ChatOpenAI(model="gpt-4.1-2025-04-14", api_key=api_key, temperature=0.2)
 llmCheap = ChatOpenAI(model="gpt-4.1-mini", api_key=api_key, temperature=0.2)
 llmNano = ChatOpenAI(model="gpt-4.1-nano-2025-04-14", api_key=api_key, temperature=0.2)
 embedding_model = OpenAIEmbeddings(model="text-embedding-3-large", api_key=api_key)
-
 
 if bool(os.getenv("USE_CHEAP_LLM_ONLY")):
     # Reassign the models to use the cheaper and non reasoning models
