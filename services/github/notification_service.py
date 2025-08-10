36eefc76863fe0b2da706083490e3f3f43208b2c
Rushat Gabhane <rushatgabhane@gmail.com>
2025-08-10 23:00:52 +0300

    update loading

--- services/github/notification_service.py
+++ services/github/notification_service.py
@@ -240,7 +240,7 @@ async def _run_command(
     if command_name == CommandName.TEST_STEPS and n.subject.type == "PullRequest":
         thinking_comment = create_thinking_comment_for_pr(
             pull_request_id=issue_or_pull_request_id,
-            thinking_text=f"{thinking_verb()}... <img src='https://github.com/user-attachments/assets/aabec0d8-4fc8-4923-909d-b258e894c4dd' height=14>",
+            thinking_text=f"{thinking_verb()}... <img src='https://github.com/user-attachments/assets/3689d0a2-6ccb-4431-8cd4-3c433c916d4a' height=16>",
         )
         async for step in test_step_pipeline.run(
             issue_or_pull_request_id, core_db, usage_log_id, thinking_comment, should_process_again=should_process_again
