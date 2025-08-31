faaa4daca8c75528bf2b55e497a3e68cf4fa1637
Rushat Gabhane <rushatgabhane@gmail.com>
2025-08-31 21:10:10 +0530

    Add GET_ISSUE query to core_queries.py

--- libs/sqlite/core/core_queries.py
+++ libs/sqlite/core/core_queries.py
@@ -139,3 +139,7 @@ INSERT_OR_UPDATE_PULL_REQUEST_REVIEW = """
 INSERT OR REPLACE INTO pull_request_reviews (pull_request_id, repo_id, last_reviewed_commit_sha, updated_at)
 VALUES (?, ?, ?, CURRENT_TIMESTAMP);
 """
+
+GET_ISSUE = """
+select title, description from issues where id=$id
+"""
