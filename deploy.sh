229746e41581eed5cde097c29884199a51091e59
Rushat Gabhane <rushatgabhane@gmail.com>
2025-08-31 21:04:52 +0530

    Enhance deploy script for automated deployment tasks

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail # exit on first error
+
+cd /home/blamegpt/blame-gpt
+git pull
+echo "pulled latest code ✅"
+
+source venv/bin/activate && pip install -r requirements.txt
+echo "installed requirements ✅"
+
+bash ./migrate.sh
+echo "ran db migrations ✅"
+
+if [ ! -f "./bin/gosec" ]; then
+    echo "installing gosec..."
+    curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s v2.22.8
+    echo "installed gosec ✅"
+else
+    echo "gosec already installed ✅"
+fi
+
+sudo systemctl restart fastapi
+echo "restarted fastapi server ✅"
 
